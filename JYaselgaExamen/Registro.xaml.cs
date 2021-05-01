db9dc562e629bf4af210f9c6fecb38eeba5404db
Edu-SSD <Edu-SSD@DESKTOP-01M5JPA>
2021-04-30 23:36:31 -0500

    Agregar archivos de proyecto.

---
+++ JYaselgaExamen/Registro.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JYaselgaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(String usuario)
+        {
+            InitializeComponent();
+
+            lblUsuario.Text = usuario;
+        }
+
+        private async void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            
+
+            double pago = Convert.ToDouble(txtPagoinicial.Text);
+            double rest = (1800 - pago) / 3;
+            double total = (rest + (1800 * 0.05)) * 3;
+            txtTotal.Text = total.ToString();
+
+            await DisplayAlert("Cálculo", "Cálculo Realizado", "OK");
+
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Guardar", "Elemento guardado con exito", "OK");
+
+            string nombre = txtNombre.Text;
+            string mensual = txtTotal.Text;
+            string user = lblUsuario.Text;
+
+            await Navigation.PushAsync(new Resumen(nombre, mensual, user));
+        }
+    }
+}
---
+++ JYaselgaExamen/Resumen.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JYaselgaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string nombre, string mensual, string user)
+        {
+            InitializeComponent();
+
+            lblNombre.Text = nombre;
+            lblMensual.Text = mensual;
+            lblUser.Text = user;
+        }
+    }
+}
